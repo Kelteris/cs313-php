1f95d9a8982ff0f9c3253b68bec3ac96070f6243
Robert Dickerson <Robert Dickerson>
2016-05-09 14:36:29 -0600

    changed homepage.php to index.php

---
+++ assignments.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+<h1>Assignments and work coming soon</h1>
+<div><a href="homepage.php">Homepage</a></div>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,9 +1,31 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<link rel="stylesheet" type="text/css" href="homepage.css">
+	<title>Robert's Homepage</title>
 </head>
-<body>
-<h1>Assignments and work coming soon</h1>
-<div><a href="homepage.php">Homepage</a></div>
+<body class="backpic backgroundcolor">
+
+<div>
+	<h1>Welcome to Robert Dickerson's Homepage!</h1>
+</div>
+<br  /><br  /><br  />
+<div class="center">
+	<p>
+		Hi, My name is Robert, I have been attending BYU-I for 
+		about two years and I am a Software Engineering major.
+		I am from Los Angelos, California and am the youngest of 5 
+		children, and am an active member of the Church of Jesus Christ
+		of Latter-Day Saints!
+	</p>
+    <p>
+		I welcome you to one of my creations, My Webpage! I 
+		have been gaining experience as a programmer and hope
+		to show you more of what I have learned	Try the link below
+		to view some of my php projects
+	</p>
+</div>
+<br  /><br  /><br  /><br  /><br  /><br  /><br  /><br  /><br  /><br  /><br  />
+<div><a href="assignments.php">Assignment Page</a></div>
 </body>
 </html>
